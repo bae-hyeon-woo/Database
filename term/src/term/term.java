dfef1388e8687c7922def610b1d76ff39dcb84b5
user <user@user-A6TBFUG>
2019-11-28 02:32:26 +0900

    insert 완료

--- term/src/term/term.java
+++ term/src/term/term.java
@@ -36,8 +36,10 @@ public class term {
 			int i=1;
 			System.out.println("menu");
 
-			System.out.println("1. select table  2.insert player  3.delete table");
-			System.out.println("4. update table ");
+			System.out.println("1.select table    2.update table     3.delete table");
+			System.out.println("4.find data       5.insert table     6.insert team");
+			System.out.println("7.insert coach,coach_contract,league,participation,team_coach");
+			System.out.println("8.insert stadium  9.insert schedule");
 
 			menu=sc.nextInt();
 
@@ -54,68 +56,14 @@ public class term {
 				while(rs.next()) {
 					i=1;
 					while(i<=col) {
-					System.out.print(rs.getString(i)+" ");
-					i++;
+						System.out.print(rs.getString(i)+" ");
+						i++;
 					}
 					System.out.println(" ");
 				}
 				break;
 
 			case 2:
-
-				System.out.println("player table 값 입력 : name,position,back_num,height,weight,contract_date");
-				sc.nextLine();
-				
-				PreparedStatement pstmt = con.prepareStatement("insert into player (name,position,back_num,height,weight,contract_date,t_name)value(?,?,?,?,?,?,?);");
-				//a=sc.next();
-				b=sc.next();
-				c=sc.next();
-				d=sc.next();
-				e=sc.next();
-				f=sc.next();
-				g=sc.next();
-				
-				sc.nextLine();
-				System.out.println("t_name 입력: ");
-				h=sc.nextLine();
-
-				
-				pstmt.setString(1,b);
-				pstmt.setString(2,c);
-				pstmt.setString(3,d);
-				pstmt.setString(4,e);
-				pstmt.setString(5,f);
-				pstmt.setString(6,g);
-				pstmt.setString(7,h);
-				r=pstmt.executeUpdate();
-				
-				
-				if(r==0) {
-					System.out.println("내용 입력에 실패하였습니다.");
-				}
-				else
-					System.out.println("내용이 입력 되었습니다.");
-				break;
-
-			case 3:
-				System.out.println("삭제할 테이블 입력 : ");
-				a=sc.next();
-				System.out.println("삭제할 조건 입력 (ex id=1 , name='무리뉴')");
-				sc.nextLine();
-				String x=sc.nextLine();
-				
-				stmt = con.createStatement();
-				r = stmt.executeUpdate("delete from "+a+" where "+ x);
-
-				if( r == 0 ){
-					System.out.println("삭제 할  내용을 찾을 수 없습니다.");
-				}else{
-					System.out.println("삭제 되었습니다.");
-
-				}
-				break;
-
-			case 4:
 				System.out.println("수정할 테이블 입력 : ");
 				a=sc.next();
 				System.out.println("수정할 내용 입력 : ");
@@ -131,7 +79,26 @@ public class term {
 				}
 				break;
 
-			case 5:
+
+			case 3:
+				System.out.println("삭제할 테이블 입력 : ");
+				a=sc.next();
+				System.out.println("삭제할 조건 입력 (ex id=1 , name='무리뉴')");
+				sc.nextLine();
+				String x=sc.nextLine();
+
+				stmt = con.createStatement();
+				r = stmt.executeUpdate("delete from "+a+" where "+ x);
+
+				if( r == 0 ){
+					System.out.println("삭제 할  내용을 찾을 수 없습니다.");
+				}else{
+					System.out.println("삭제 되었습니다.");
+
+				}
+				break;
+
+			case 4:
 				System.out.println("검색할 테이블 입력 : ");
 				a=sc.next();
 				System.out.println("검색할 내용 입력 : ");
@@ -148,7 +115,142 @@ public class term {
 						i++;
 					}
 				break;
+
+			case 5:
+
+				System.out.println("player table 값 입력 : name,position,back_num,height,weight,contract_date");
+				sc.nextLine();
+
+				PreparedStatement pstmt = con.prepareStatement("insert into player(name,position,back_num,height,weight,contract_date,t_name)value(?,?,?,?,?,?,?);");
+				b=sc.next();
+				c=sc.next();
+				d=sc.next();
+				e=sc.next();
+				f=sc.next();
+				g=sc.next();
+
+				sc.nextLine();
+				System.out.println("t_name 입력: ");
+				h=sc.nextLine();
+
+				pstmt.setString(1,b);
+				pstmt.setString(2,c);
+				pstmt.setString(3,d);
+				pstmt.setString(4,e);
+				pstmt.setString(5,f);
+				pstmt.setString(6,g);
+				pstmt.setString(7,h);
+				r=pstmt.executeUpdate();
+
+
+				if(r==0) {
+					System.out.println("내용 입력에 실패하였습니다.");
+				}
+				else
+					System.out.println("내용이 입력 되었습니다.");
+				break;
+				
+			case 6:
+				System.out.println("team table 값 입력 : team_name, frm, stadium, c_name");
+				sc.nextLine();
+				pstmt = con.prepareStatement("insert into team value(?,?,?,?);");
+				System.out.print("팀이름 입력 : ");
+				b=sc.nextLine();
+				System.out.print("연고지 입력 : ");
+				c=sc.nextLine();
+				System.out.print("구 입력 : ");
+				d=sc.nextLine();
+				System.out.print("감독이름 입력 : ");
+				e=sc.nextLine();
+
+				pstmt.setString(1,b);
+				pstmt.setString(2,c);
+				pstmt.setString(3,d);
+				pstmt.setString(4,e);
+				r=pstmt.executeUpdate();
+				if(r==0) {
+					System.out.println("내용 입력에 실패하였습니다.");
+				}
+				else
+					System.out.println("내용이 입력 되었습니다.");
+				break;
+			case 7:
+				System.out.println("table 입력 (league, coach ,team_coach, coach_contract, paricipation 만 가능)");
+				a=sc.next();
+				
+				pstmt = con.prepareStatement("insert into "+a+" value(?,?);");
+				sc.nextLine();
+				System.out.println("칼럼1 값 입력 : ");
+				b=sc.nextLine();
+				System.out.println("칼럼2 값 입력 : ");
+				c=sc.nextLine();
+				pstmt.setString(1,b);
+				pstmt.setString(2,c);
+				r=pstmt.executeUpdate();
+				if(r==0) {
+					System.out.println("내용 입력에 실패하였습니다.");
+				}
+				else
+					System.out.println("내용이 입력 되었습니다.");
+				break;
+			case 8:
+				System.out.println("stadium table 값 입력 : stadium_name, address, ht_name");
+				sc.nextLine();
+				pstmt = con.prepareStatement("insert into stadium value(?,?,?);");
+				System.out.print("구장 이름 입력 : ");
+				b=sc.nextLine();
+				System.out.print("구장 주소 입력 : ");
+				c=sc.nextLine();
+				System.out.print("홈 팀 이름 입력 : ");
+				d=sc.nextLine();
+				
+
+				pstmt.setString(1,b);
+				pstmt.setString(2,c);
+				pstmt.setString(3,d);
+				r=pstmt.executeUpdate();
+				if(r==0) {
+					System.out.println("내용 입력에 실패하였습니다.");
+				}
+				else
+					System.out.println("내용이 입력 되었습니다.");
+				break;
+			case 9:
+				System.out.println("schedule table 값 입력 : s_num, h_tm, a_tm, h_score, a_score, stadium, schedule, league_name");
+				
+				pstmt = con.prepareStatement("insert into schedule (h_tm,a_tm,h_score,a_score,stadium,schedule,league_name) value(?,?,?,?,?,?,?);");
+				sc.nextLine();
+				System.out.print("홈 팀 이름 입력 : ");
+				b=sc.nextLine();
+				System.out.print("어웨이 팀 이름 입력 : ");
+				c=sc.nextLine();
+				System.out.print("홈 팀 스코어 입력 : ");
+				d=sc.nextLine();
+				System.out.print("어웨이팀 스코어 입력 : ");
+				e=sc.nextLine();
+				System.out.print("구장 입력 : ");
+				f=sc.nextLine();
+				System.out.print("일정 입력 : ");
+				g=sc.nextLine();
+				System.out.print("리그 이름 입력 : ");
+				h=sc.nextLine();
+				
+				pstmt.setString(1,b);
+				pstmt.setString(2,c);
+				pstmt.setString(3,d);
+				pstmt.setString(4,e);
+				pstmt.setString(5,f);
+				pstmt.setString(6,g);
+				pstmt.setString(7,h);
+				r=pstmt.executeUpdate();
+				if(r==0) {
+					System.out.println("내용 입력에 실패하였습니다.");
+				}
+				else
+					System.out.println("내용이 입력 되었습니다.");
+				break;
 			}
+				
 			con.close();
 		}catch(Exception e) {
 			System.out.println(e);
